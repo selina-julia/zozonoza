31fcc5191345f58246f0457b1cf3a5995d94c14e
selina-julia <79986438+selina-julia@users.noreply.github.com>
2021-06-14 19:55:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".testimonial .indicators li").click(function() {
+    var i = $(this).index();
+    var targetElement = $(".testimonial .tabs li");
+    targetElement.eq(i).addClass("active");
+    targetElement.not(targetElement[i]).removeClass("active");
+  });
+  $(".testimonial .tabs li").click(function() {
+    var targetElement = $(".testimonial .tabs li");
+    targetElement.addClass("active");
+    targetElement.not($(this)).removeClass("active");
+  });
+});
+$(document).ready(function() {
+  $(".slider .swiper-pagination span").each(function(i) {
+    $(this)
+      .text(i + 1)
+      .prepend("0");
+  });
+});
+
+$(function() {
+  var $body = $(document);
+  $body.bind("scroll", function() {
+    if ($body.scrollLeft() !== 0) {
+      $body.scrollLeft(0);
+    }
+  });
+});
